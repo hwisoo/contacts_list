1e435df700e0890cc35b1e813e52740ef5dc3a9f
James Cho <hwisoocho@gmail.com>
2018-08-10 15:08:01 -0700

    first commit

---
+++ contacts.rb
@@ -0,0 +1,21 @@
+def ask(question, kind="string")
+    print question + " "
+    answer = gets.chomp
+    answer = answer.to_i if kind=="number"
+    return answer
+end
+
+def add_contact
+    contact = {"name" =>"", "phone_numbers" => []}
+    contact["name"] = ask("What is the person's name?")
+    answer = ""
+    while answer !="n"
+        answer = ask("Do you want to add a phone number?(y/n)")
+        if answer =="y"
+            phone = ask("Enter a phone number:")
+            contact["phone_numbers"].push(phone)
+        end
+    end
+    return contact
+end
+
